c708154299c720ce1f75087401f384e20fec445e
Birinyem4 <omotolairinyemi4@gmail.com>
2024-03-03 03:20:23 -0600

    add

---
+++ terraform/ec2/data_sources.tf
@@ -0,0 +1,16 @@
+data "aws_subnet" "private" {
+  count = local.private_subnet_count  # Use the count parameter here
+
+  cidr_block = var.private_subnet_cidr_blocks[count.index]
+  vpc_id     = data.aws_vpc.main.id
+
+  tags = {
+    Name = "privatesubnet-${count.index}"
+  }
+}
+
+data "aws_vpc" "main" {
+    tags = {
+    name = "vpc"
+  }
+}
