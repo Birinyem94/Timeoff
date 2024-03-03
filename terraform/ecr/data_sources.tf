3e7b48653477e989473b8c7c8698468894084a07
Birinyem4 <omotolairinyemi4@gmail.com>
2024-03-03 03:38:54 -0600

    add files

---
+++ terraform/ecr/data_sources.tf
@@ -0,0 +1,29 @@
+   # Task logging privileges
+data "aws_iam_policy_document" "ecr" {
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::9891377060995:root" 
+      ]
+    }
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings"
+    ]
+  }
+}
