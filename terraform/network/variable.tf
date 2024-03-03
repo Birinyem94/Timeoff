4fc98479e62d872c87ab73a77793df7fee828eed
Birinyem4 <omotolairinyemi4@gmail.com>
2024-03-03 03:38:24 -0600

    add files

--- terraform/network/variable.tf
+++ terraform/network/variable.tf
@@ -26,7 +26,7 @@ variable "public_subnet_cidr_blocks" {
 }
 
 variable "private_subnet_cidr_blocks" {
-  description = "VPC Public Subnets"
+  description = "VPC Private Subnets"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
--- terraform/network/vpc.tf
+++ terraform/network/vpc.tf
@@ -17,24 +17,25 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnet_cidr_blocks)
+  count = 2
 
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidr_blocks[count.index]
   availability_zone = var.availability_zones[count.index]
   tags = {
-    Name = "privatesubnet-${count.index}"
+    Name = "privatesubnet"
   }
 }
 
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnet_cidr_blocks)
+ count = 2
+
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr_blocks[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = "publicsubnet-${count.index}"
+    Name = "publicsubnet"
   }
 }
 
